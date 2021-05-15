7289689d4d40cadca7741150edb243a0615fd80c
sherrycheungw <79497299+sherrycheungw@users.noreply.github.com>
2021-05-15 18:59:57 +0800

    Update Header.vue

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -11,8 +11,8 @@
                 </g-link>
             </div>
             <nav class="nav right">
-                <g-link class="nav__link" to="/journal">Journal</g-link>
-                <g-link class="nav__link" to="/contact">Say Hi!</g-link>
+                <g-link class="nav__link" to="/journal">My Blog</g-link>
+                <g-link class="nav__link" to="/contact">Enquire</g-link>
             </nav>
         </div>
     </header>
