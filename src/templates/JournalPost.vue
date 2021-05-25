c2ecf8c4801d7a298b473032de16f9c875195836
sherrycheungw <79497299+sherrycheungw@users.noreply.github.com>
2021-05-25 14:08:52 +0800

    Update JournalPost.vue

--- src/templates/JournalPost.vue
+++ src/templates/JournalPost.vue
@@ -5,20 +5,6 @@
 
         <div class="journal-header">
           <h1 v-html="$page.post.title" class="journal-title" />
-          <div class="journal-meta">
-            <div class="journal-author">
-              <span class="label">Author</span>
-              <span class="author-name" v-text="$page.post.author" />
-            </div>
-            <div class="journal-date">
-              <span class="label">Date</span>
-              <div v-text="$page.post.date"/>
-            </div>
-            <div class="journal-time">
-              <span class="label">Time</span>
-              <span>{{ $page.post.timeToRead }} min read</span>
-            </div>
-          </div>          
         </div>
 
         <JournalContent :content="$page.post.content" />
