31bc66c7d4b9f37cdb000647b07dba1431cf9db5
sherrycheungw <79497299+sherrycheungw@users.noreply.github.com>
2021-06-02 14:00:56 +0800

    Update Journal.vue

--- src/pages/Journal.vue
+++ src/pages/Journal.vue
@@ -4,7 +4,7 @@
     <div class="container">
       <div class="journal-hero">
         <h1 class="journal-header">
-          a wise person once said...
+          the spaces we have recreated...
         </h1>
       </div>
     </div>
